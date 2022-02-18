ffcf452a3f6e5f35e877710a79d6dc60b9c64a40
adriensimon16 <91315213+adriensimon16@users.noreply.github.com>
2022-02-18 16:56:35 +0100

    update sudoku

--- Sudoku.R
+++ Sudoku.R
@@ -3,3 +3,13 @@ QuelBloc <- function(a,b){
 }
 
 
+existeSurBloc <- function(x, grille, a){
+  for(i in 1:3){
+    for(j in 1:3){
+      if(grille[a[1]*3+i][a[2]*3+j]==x){
+        return(TRUE)
+      }
+    }
+  }
+  return(FALSE)
+}
